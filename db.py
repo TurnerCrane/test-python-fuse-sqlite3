c147727fff5ed278b58541b6a87e715f6c460fd7
TurnerCrane <tana.public.xero@gmail.com>
2024-10-04 01:02:11 +0900

    Update

--- db.py
+++ db.py
@@ -1,4 +1,5 @@
 import sqlite3
+import time
 
 
 GROUPS_TABLE_SQL = """
@@ -15,7 +16,13 @@ CREATE TABLE IF NOT EXISTS datas (
     data_id INTEGER PRIMARY KEY AUTOINCREMENT,
     group_id INTEGER,
     name TEXT,
+    gid INTEGER,
+    uid INTEGER,
+    mode INTEGER,
     data BLOB,
+    atime REAL,
+    mtime REAL,
+    ctime REAL,
     UNIQUE(group_id, name),
     foreign key (group_id) references groups(group_id)
 )
@@ -24,7 +31,8 @@ CREATE TABLE IF NOT EXISTS datas (
 
 class DB:
     def __init__(self, path):
-        self.conn = sqlite3.connect(path, check_same_thread=(False if sqlite3.threadsafety == 3 else True))
+        check_same_thread = (sqlite3.threadsafety != 3)
+        self.conn = sqlite3.connect(path, check_same_thread=check_same_thread)
         self.ensure_tables()
 
     def ensure_tables(self):
@@ -33,23 +41,42 @@ class DB:
             self.conn.execute(DATAS_TABLE_SQL)
 
     def is_exists_group(self, group_name):
-        exists = self.conn.execute("SELECT 1 from groups WHERE name = ?;", (group_name,)).fetchone()
+        exists = self.conn.execute(
+            """
+            SELECT 1 from groups WHERE name = ?;
+            """, (group_name,)
+        ).fetchone()
         return not (exists is None)
 
     def is_exists_data(self, group_name, data_name):
-        exists = self.conn.execute("SELECT 1 FROM datas WHERE group_id IN (SELECT group_id FROM groups WHERE name = ?) AND name = ?;", (group_name, data_name)).fetchone()
+        exists = self.conn.execute(
+            """
+            SELECT 1 FROM datas WHERE group_id IN
+            (SELECT group_id FROM groups WHERE name = ?) AND name = ?;
+            """, (group_name, data_name)
+        ).fetchone()
         return not (exists is None)
-    
+
     def iter_group_names(self):
         for row in self.conn.execute("SELECT name FROM groups"):
             yield row[0]
-    
+
     def iter_group_data_names(self, group_name):
-        for row in self.conn.execute("SELECT name FROM datas WHERE group_id IN (SELECT group_id FROM groups WHERE name = ?)", (group_name,)):
+        for row in self.conn.execute(
+            """
+            SELECT name FROM datas WHERE group_id IN
+            (SELECT group_id FROM groups WHERE name = ?)
+            """, (group_name,)
+        ):
             yield row[0]
 
     def _get_data_rowid(self, group_name, data_name):
-        result = self.conn.execute("SELECT rowid FROM datas WHERE group_id IN (SELECT group_id FROM groups WHERE name = ?) AND name = ?;", (group_name, data_name)).fetchone()
+        result = self.conn.execute(
+            """
+            SELECT rowid FROM datas WHERE group_id IN
+            (SELECT group_id FROM groups WHERE name = ?) AND name = ?;
+            """, (group_name, data_name)
+        ).fetchone()
         if not result:
             return
         return result[0]
@@ -60,8 +87,19 @@ class DB:
             return
 
         return self.conn.blobopen("datas", "data", rowid)
-    
-    def write_data(self, group_name, data_name, data, offset=0):
+
+    def get_data_mode(self, group_name, data_name):
+        rowid = self._get_data_rowid(group_name, data_name)
+        if rowid is None:
+            return None
+        mode = self.conn.execute(
+            """
+            SELECT mode FROM datas WHERE rowid = ?
+            """, (rowid,)
+        ).fetchone()
+        return mode[0] if mode else None
+
+    def write_data_org(self, group_name, data_name, data, offset=0):
         with self.conn as conn:
             old = self.get_data(group_name, data_name).read()
 
@@ -76,17 +114,151 @@ class DB:
                 blob.write(old)
                 blob[offset:] = data
 
+    def write_data(self, group_name, data_name, data, offset=0):
+        with self.conn as conn:
+            # 現在のデータを取得
+            old_data = self.get_data(group_name, data_name).read()
+            # 新しいサイズを計算
+            new_size = max(len(old_data), offset + len(data))
+            self.clear_data(group_name, data_name, new_size)
+
+            rowid = self._get_data_rowid(group_name, data_name)
+            if not rowid:
+                return
+
+            # データを書き込む
+            with conn.blobopen("datas", "data", rowid) as blob:
+                blob.write(
+                    old_data[:offset] + data + old_data[offset + len(data):]
+                )
+
     def clear_data(self, group_name, data_name, size=0):
         with self.conn as conn:
-            conn.execute("UPDATE datas SET data = zeroblob(?) WHERE group_id IN (SELECT group_id FROM groups WHERE name = ?) AND name = ?;", (size, group_name, data_name))
+            conn.execute(
+                """
+                UPDATE datas SET data = zeroblob(?) WHERE group_id IN
+                (SELECT group_id FROM groups WHERE name = ?) AND name = ?;
+                """, (size, group_name, data_name)
+            )
 
     def create_group(self, group_name):
         with self.conn as conn:
-            conn.execute("INSERT INTO groups(name) VALUES(?);", (group_name,))
+            conn.execute(
+                """
+                INSERT INTO groups(name) VALUES(?);
+                """, (group_name,)
+            )
 
-    def create_data(self, group_name, data_name):
+    def create_data(self, group_name, data_name, gid=0, uid=0, mode=0o644):
+        now = time.time()
         with self.conn as conn:
-            conn.execute("INSERT INTO datas(group_id, name, data) VALUES((SELECT group_id FROM groups WHERE name = ?), ?, zeroblob(0));", (group_name, data_name))
+            conn.execute(
+                """
+                INSERT INTO datas(
+                    group_id, name, gid, uid, mode, atime, mtime, ctime, data
+                )
+                VALUES(
+                    (SELECT group_id FROM groups WHERE name = ?),
+                    ?, ?, ?, ?, ?, ?, ?, zeroblob(0)
+                );
+                """, (group_name, data_name, gid, uid, mode, now, now, now)
+            )
+
+    def set_data_permissions(self, group_name, data_name, mode):
+        rowid = self._get_data_rowid(group_name, data_name)
+        if rowid is None:
+            return None
+
+        with self.conn as conn:
+            conn.execute(
+                """
+                UPDATE datas SET mode = ? WHERE rowid = ?
+                """, (mode, rowid)
+            )
+
+    def get_data_owner(self, group_name, data_name):
+        rowid = self._get_data_rowid(group_name, data_name)
+        if rowid is None:
+            return None
+
+        row = self.conn.execute(
+            """
+            SELECT gid, uid FROM datas WHERE rowid = ?
+            """, (rowid, )
+        ).fetchone()
+        return row if row else None
+
+    def set_data_owner(self, group_name, data_name, uid, gid):
+        rowid = self._get_data_rowid(group_name, data_name)
+        if rowid is None:
+            return None
+
+        with self.conn as conn:
+            conn.execute(
+                """
+                UPDATE datas SET uid = ?, gid = ? WHERE rowid = ?
+                """, (uid, gid, rowid))
+
+    def delete_group(self, group_name):
+        with self.conn as conn:
+            conn.execute(
+                """
+                DELETE FROM groups WHERE group_id IN
+                (SELECT group_id FROM groups WHERE name = ?);
+                """, (group_name,)
+            )
+
+    def delete_data(self, group_name, data_name):
+        with self.conn as conn:
+            conn.execute(
+                """
+                DELETE FROM datas WHERE group_id IN
+                (SELECT group_id FROM groups WHERE name = ?) AND name = ?;
+                """, (group_name, data_name)
+            )
+
+    def get_timestamps(self, group_name, data_name):
+        rowid = self._get_data_rowid(group_name, data_name)
+        if rowid is None:
+            return None
+
+        row = self.conn.execute(
+            """
+            SELECT atime, mtime, ctime FROM datas WHERE rowid = ?;
+            """, (rowid,)
+        ).fetchone()
+        return row if row else None
+        # if result:
+        #     atime, mtime, ctime = result
+        #     return atime, mtime, ctime
+        # else:
+        #     return None, None, None
+
+    def update_timestamps(self, group_name, data_name, atime=None, mtime=None, ctime=None):
+        rowid = self._get_data_rowid(group_name, data_name)
+        if rowid is None:
+            return None
+
+        with self.conn as conn:
+            if atime is not None:
+                conn.execute(
+                    """
+                    UPDATE datas SET atime = ? WHERE rowid = ?
+                    """, (atime, rowid)
+                )
+            if mtime is not None:
+                conn.execute(
+                    """
+                    UPDATE datas SET mtime = ? WHERE rowid = ?
+                    """, (mtime, rowid)
+                )
+            if ctime is not None:
+                conn.execute(
+                    """
+                    UPDATE datas SET ctime = ? WHERE rowid = ?
+                    """, (ctime, rowid)
+                )
+
 
 if __name__ == '__main__':
     db = DB(":memory:")
@@ -97,5 +269,8 @@ if __name__ == '__main__':
     print(list(db.iter_group_names()))
     print(list(db.iter_group_data_names("test")))
     print(db.get_data("test", "test").read())
-    print(db.conn.execute("SELECT json_extract(data, '$.b') FROM datas;").fetchall())
+    print(db.conn.execute(
+            "SELECT json_extract(data, '$.b') FROM datas;"
+            ).fetchall()
+    )
     breakpoint()
--- main.py
+++ main.py
@@ -1,13 +1,17 @@
+import errno
+import logging
 import os
 import stat
-import errno
+import time
+
 import fuse
 from fuse import Fuse
-import os.path
+
 from db import DB
 
 
 fuse.fuse_python_api = (0, 2)
+logging.basicConfig(filename='/app/app.log', level=logging.DEBUG, format='%(asctime)s - %(levelname)s - %(message)s')
 
 
 class Stat(fuse.Stat):
@@ -19,9 +23,9 @@ class Stat(fuse.Stat):
         self.st_uid = 0
         self.st_gid = 0
         self.st_size = 0
-        self.st_atime = 0
-        self.st_mtime = 0
-        self.st_ctime = 0
+        self.st_atime = time.time()
+        self.st_mtime = time.time()
+        self.st_ctime = time.time()
         for k, v in kwargs.items():
             setattr(self, k, v)
 
@@ -49,35 +53,60 @@ class DBFS(Fuse):
 
     def getattr(self, path):
         group_name, data_name = _split_data_path(path)
-        print(f"getattr: path={path}, group={group_name}, data={data_name}")
+        logging.info(f"getattr: path={path}, group={group_name}, data={data_name}")
 
         # request '/' attr
         if group_name is None and data_name is None:
-            return Stat(st_mode=stat.S_IFDIR|0o755, st_nlink=2)
+            return Stat(st_mode=(stat.S_IFDIR | 0o755), st_nlink=2)
 
         # request '/<group_name>'
         elif group_name is not None and data_name is None:
             if not self.db.is_exists_group(group_name):
                 return -errno.ENOENT
-            return Stat(st_mode=stat.S_IFDIR|0o755, st_nlink=1)
+            return Stat(st_mode=(stat.S_IFDIR | 0o755), st_nlink=1)
 
         # request '/<group_name>/<data_name>'
         elif group_name is not None and data_name is not None:
             if not self.db.is_exists_data(group_name, data_name):
-                print(group_name, data_name, "not exist")
+                logging.debug(f"getattr: {group_name}, {data_name} not exist")
                 return -errno.ENOENT
 
+            row = self.db.get_data_owner(group_name, data_name)
+            # row = self.db.conn.execute("SELECT gid, uid FROM datas WHERE group_id IN (SELECT group_id FROM groups WHERE name = ?) AND name = ?;",(group_name, data_name)).fetchone()
+            # loggin.debug(f"getattr: row={row}")
+
+            if row:
+                gid, uid = row
+            else:
+                return -errno.ENOENT
+            logging.debug(f"getattr: gid={gid}, uid={uid}")
+
             size = len(self.db.get_data(group_name, data_name))
-            return Stat(st_mode=stat.S_IFREG|0o644, st_nlink=1, st_size=size)
-        
+            mode = self.db.get_data_mode(group_name, data_name)
+
+            timestamp = self.db.get_timestamps(group_name, data_name)
+            if timestamp:
+                atime, mtime, ctime = timestamp
+            else:
+                atime, mtime, ctime = (None, None, None)
+            # atime, mtime, ctime = self.db.get_timestamps(group_name, data_name)
+            logging.debug(f"getattr: atime={atime}, mtime={mtime}, ctime={ctime}")
+
+            # FIXME(なんか複雑)
+            if atime is None or mtime is None or ctime is None:
+                now = time.time()
+                self.db.update_timestamps(group_name, data_name, atime=now, mtime=now, ctime=now)
+                return Stat(st_mode=(stat.S_IFREG | mode), st_nlink=1, st_size=size, st_gid=gid, st_uid=uid, st_atime=now, st_mtime=now, st_ctime=now)
+            else:
+                return Stat(st_mode=(stat.S_IFREG | mode), st_nlink=1, st_size=size, st_gid=gid, st_uid=uid, st_atime=atime, st_mtime=mtime, st_ctime=ctime)
         else:
             return -errno.ENOENT
 
     def readdir(self, path, offset):
         group_name, data_name = _split_data_path(path)
-        print(f"readdir: path={path}, offset={offset}, group={group_name}, data={data_name}")
+        logging.info(f"readdir: path={path}, offset={offset}, group={group_name}, data={data_name}")
 
-        for r in  '.', '..':
+        for r in ['.', '..']:
             yield fuse.Direntry(r)
 
         # request '/<group_name>'
@@ -92,7 +121,7 @@ class DBFS(Fuse):
 
     def open(self, path, flags):
         group_name, data_name = _split_data_path(path)
-        print(f"open: path={path}, flags={flags}, group={group_name}, data={data_name}")
+        logging.info(f"open: path={path}, flags={flags}, group={group_name}, data={data_name}")
 
         if group_name and not self.db.is_exists_group(group_name):
             return -errno.ENOENT
@@ -100,10 +129,9 @@ class DBFS(Fuse):
         if data_name and not self.db.is_exists_data(group_name, data_name):
             return -errno.ENOENT
 
-
     def read(self, path, size, offset):
         group_name, data_name = _split_data_path(path)
-        print(f"read: path={path}, size={size}, offset={offset}, group={group_name}, data={data_name}")
+        logging.info(f"read: path={path}, size={size}, offset={offset}, group={group_name}, data={data_name}")
 
         data = self.db.get_data(group_name, data_name)
         datalen = len(data)
@@ -119,35 +147,83 @@ class DBFS(Fuse):
 
     def write(self, path, buf, offset):
         group_name, data_name = _split_data_path(path)
-        print(f"read: path={path}, buf=({len(buf)}), offset={offset}, group={group_name}, data={data_name}")
+        logging.info(f"write: path={path}, buf=({len(buf)}), offset={offset}, group={group_name}, data={data_name}")
 
         self.db.write_data(group_name, data_name, buf, offset=offset)
+
+        now = time.time()
+        self.db.update_timestamps(group_name, data_name, mtime=now)
+
         return len(buf)
 
     def truncate(self, path, size):
         group_name, data_name = _split_data_path(path)
-        print(f"truncate: path={path}, size={size}, group={group_name}, data={data_name}")
-        self.db.clear_data(group_name, data_name)
+        logging.info(f"truncate: path={path}, size={size}, group={group_name}, data={data_name}")
+        # FIXME(viで変更するとファイルサイズがおかしい)
+        # self.db.clear_data(group_name, data_name)
 
     def create(self, path, flags, mode):
         group_name, data_name = _split_data_path(path)
-        print(f"create: path={path}, flags={flags}, mode={mode}, group={group_name}, data={data_name}")
+        logging.info(f"create: path={path}, flags={flags}, mode={mode}, group={group_name}, data={data_name}")
+
         self.db.create_data(group_name, data_name)
+
+        now = time.time()
+        self.db.update_timestamps(group_name, data_name, atime=now, mtime=now, ctime=now)
 
     def mkdir(self, path, mode):
         group_name, data_name = _split_data_path(path)
-        print(f"create: path={path}, mode={mode}, group={group_name}, data={data_name}")
+        logging.info(f"mkdir: path={path}, mode={mode}, group={group_name}, data={data_name}")
+
         self.db.create_group(group_name)
+
+    def rmdir(self, path):
+        group_name, _ = _split_data_path(path)
+        logging.info(f"rmdir: path={path}, group={group_name}")
+        self.db.delete_group(group_name)
+
+    def unlink(self, path):
+        group_name, data_name = _split_data_path(path)
+        logging.info(f"unlink: path={path}, group={group_name}, data={data_name}")
+        self.db.delete_data(group_name, data_name)
+
+    def chmod(self, path, mode):
+        group_name, data_name = _split_data_path(path)
+        logging.info(f"chmod: path={path}, mode={mode}, group={group_name}, data={data_name}")
+
+        if data_name:
+            self.db.set_data_permissions(group_name, data_name, mode)
+            self.db.update_timestamps(group_name, data_name, ctime=time.time())
+            logging.debug(f"chmod: permissions changed for {data_name} in group {group_name} to {mode}")
+        else:
+            logging.warning(f"chmod: permission change for directory {group_name} is not allowed")
+            return -errno.EPERM
+
+    def chown(self, path, gid, uid):
+        group_name, data_name = _split_data_path(path)
+        logging.info(f"chown: path={path}, gid={gid}, uid={uid}, group={group_name}, data={data_name}")
+
+        if data_name and self.db.is_exists_data(group_name, data_name):
+            # if not self.db.set_data_owner(group_name, data_name, gid, uid):
+            #     return -errno.ENOENT
+            self.db.set_data_owner(group_name, data_name, gid, uid)
+            self.db.update_timestamps(group_name, data_name, ctime=time.time())
+            logging.info(f"chown: Owners changed for {data_name} in group {group_name} to {gid}:{uid}")
+        else:
+            logging.error(f"chown: {path} does not exist")
+            return -errno.EPERM
 
 
 def main():
-    usage="DBFS" + Fuse.fusage
-    server = DBFS("data.db", version="%prog " + fuse.__version__,
-                     usage=usage,
-                     dash_s_do='setsingle')
+    usage = "DBFS" + Fuse.fusage
+    server = DBFS("data.db",
+                  version="%prog " + fuse.__version__,
+                  usage=usage,
+                  dash_s_do='setsingle')
 
     server.parse(errex=1)
     server.main()
+
 
 if __name__ == '__main__':
     main()
